97e0d43e62eb59f809ea3c7dcddf58a477ce2860
chauthutran <cthutran@gmail.com>
2022-06-20 23:59:49 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,13 +4,16 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
+
+
+const clientURL = "https://client-dev.psi-connect.org";
 const INDEX = '/index.html';
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const io = socketIO(server);
+const io = require('socket.io')(server);
 
 io.on('connection', (socket) => {
   console.log('Client connected');
