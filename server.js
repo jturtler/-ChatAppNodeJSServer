dbd97f1ca67027bd6d980f0e82a840f1ef83ff14
chauthutran <cthutran@gmail.com>
2022-06-21 00:05:35 +0900

    Update server.js

--- server.js
+++ server.js
@@ -13,9 +13,17 @@ const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const io = require('socket.io')(server);
+const io = require('socket.io')(server,{
+  cors: {
+		origin: clientURL,
+		methods: ["GET", "POST"],
+		credentials: true
+	}
+});
 
 io.on('connection', (socket) => {
+  socket.emit('connect success', {connectstatus: "success"} );
+
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
