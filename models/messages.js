030b697cc8c776af5b1c7aad166e252adfa14772
chauthutran <cthutran@gmail.com>
2022-06-20 11:25:55 +0900

    Init app

---
+++ models/messages.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const msgSchema = new mongoose.Schema({
+    sender: {
+        type: String,
+        required: true
+    },
+	receiver: {
+        type: String,
+        required: true
+    },
+	msg: {
+		type: String,
+		required: true
+	},
+	datetime: {
+		type: String,
+		required: true
+	},
+	filetype: {
+		type: String,
+		required: false
+	}
+})
+
+const MessagesCollection = mongoose.model('messages', msgSchema);
+module.exports = MessagesCollection;
