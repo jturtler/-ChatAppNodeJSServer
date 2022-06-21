43cf5ac034c0d5dafbf3799d0721a3eb419c7984
chauthutran <cthutran@gmail.com>
2022-06-21 13:26:29 +0900

    Change server port to 3111

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const socketIO = require('socket.io');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3111;
 
 
 const clientURL = "https://client-dev.psi-connect.org";
